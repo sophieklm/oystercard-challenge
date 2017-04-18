f2ebaf3df91439466f2550eb796eeb62bf9f6e08
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-04-18 17:41:33 +0100

    refactored @balance to self.balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,11 @@
 class Oystercard
   attr_accessor :balance
-  
+
   def initialize (balance = 0)
     @balance = balance
   end
 
   def top_up(amount = 0)
-    @balance += amount 
+    self.balance += amount 
   end
 end
