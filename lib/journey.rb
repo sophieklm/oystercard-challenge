24cc13f22c0fe901f8bd921cca48fd50af9f3309
Sophie Mann <sklm@live.co.uk>
2017-04-21 15:04:55 +0100

    initializes journey with station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,6 +1,16 @@
 
 class Journey
 
+  attr_reader :entry_station, :exit_station
+
+  def initialize(station)
+    @entry_station = station
+  end
+
+  def start_journey(station)
+
+  end
+
+
 
 end
-
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,6 +1,22 @@
 require 'journey'
 
 
-define Journey do
+describe Journey do
+  subject {described_class.new(:bank)}
+  it 'has 1 argument' do
+    expect(Journey).to respond_to(:new).with(1).argument
+  end
+
+  it 'starts a journey' do
+    expect(subject).to respond_to(:start_journey).with(1).argument
+  end
+
+  it 'has an entry station' do
+    expect(subject.entry_station).to eq :bank
+  end
+
+  xit 'has an exit station' do
+    expect(subject.exit_station).to eq :bank
+  end
 
 end
